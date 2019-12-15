9e1a3dc50d0dad59ffa3911e616e9883177c0b10
Gonçalo Henriques <gonelf@gmail.com>
2019-12-15 18:44:15 +0000

    add logrocket identifier

---
+++ js/logrocket.js
@@ -0,0 +1,22 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+if (getUrlParameter("private") == true) {
+  console.log("identified");
+  LogRocket.identify('1', {
+    name: 'gonelf',
+    admin: 'true'
+  });
+}
