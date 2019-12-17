2dbc2320909dca61bdd7ccf4d84668062018877e
Gonçalo Henriques <gonelf@gmail.com>
2019-12-17 10:33:50 +0000

    update logrocket call

--- js/logrocket.js
+++ js/logrocket.js
@@ -14,5 +14,9 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 if (getUrlParameter("private") != "true") {
-  $("head").append('<script src="https://cdn.lr-ingest.io/LogRocket.min.js" crossorigin="anonymous"></script><script>window.LogRocket && window.LogRocket.init("vs91x2/request-for-product");</script>')
+  //$("head").append('<script src="https://cdn.lr-ingest.io/LogRocket.min.js" crossorigin="anonymous"></script><script>window.LogRocket && window.LogRocket.init("vs91x2/request-for-product");</script>')
+  $.getScript( "https://cdn.lr-ingest.io/LogRocket.min.js", function( data, textStatus, jqxhr ) {
+    console.log( "LogRocket" );
+    window.LogRocket && window.LogRocket.init("vs91x2/request-for-product");
+  });
 }
