ec8a504943445e9c4eabe0a364d3da8dba98378e
Gonçalo Henriques <gonelf@gmail.com>
2019-12-18 14:21:00 +0000

    v2.0

--- js/previousIssues.js
+++ js/previousIssues.js
@@ -7,8 +7,8 @@ $.getJSON( "https://script.google.com/macros/s/AKfycbwq0DNju8avFVO6JUjiFt_gXASf7
                     '<div class="card-body">'+
                       '<p class="card-text">' + i_val["description"] + '</p>'+
                       '<p class="card-text text-right">'+
-                        '<small class="text-muted">'+
-                          '<a href="' + i_val['original_link'] + '" target="_blank" class="card-link">' + i_val['user'] + ' on ' + i_val['date'] + '</a>'+
+                        '<small>'+
+                          '<a href="' + i_val['original_link'] + '" target="_blank" class="turquoise" style="text-decoration: none;">' + i_val['user'] + ' on ' + i_val['date'] + '</a>'+
                         '</small>'+
                       '</p>'+
                     '</div>'+
